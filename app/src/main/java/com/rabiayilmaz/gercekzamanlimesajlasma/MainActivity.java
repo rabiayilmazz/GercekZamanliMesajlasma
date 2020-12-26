549a095c181fc68167561a77d946bfc20d3ecb8e
rabia yılmaz <rabiarabiabbb@gmail.com>
2020-12-26 16:19:56 +0300

    Loglama
    Mesajlaşma log

--- app/src/main/java/com/rabiayilmaz/gercekzamanlimesajlasma/MainActivity.java
+++ app/src/main/java/com/rabiayilmaz/gercekzamanlimesajlasma/MainActivity.java
@@ -1,16 +1,72 @@
 package com.rabiayilmaz.gercekzamanlimesajlasma;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
+    List<String> list;
+    String username;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toast.makeText(getApplicationContext(),"Başarılı bir şekilde kayıt yapılmıştır.",Toast.LENGTH_SHORT).show();
+        tanimla();
+        listele();
     }
 
+    public void tanimla(){
+        username = getIntent().getExtras().getString("kadi");
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        reference = firebaseDatabase.getReference();
+    }
+    public void listele(){
+        reference.child("Kullanıcılar").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(!dataSnapshot.getKey().equals(username)) {
+                    list.add(dataSnapshot.getKey());
+                    Log.i("Kullanıcılar",dataSnapshot.getKey());
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
